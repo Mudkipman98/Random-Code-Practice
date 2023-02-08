bc9a68c7e5a32d888b784aa1063ef34e7c4dbcaa
Stephan DeWitz <mudkipman98@gmail.com>
2023-02-07 21:58:24 -0800

    Added Tools.cs to organize frequently used methods.

---
+++ C# Practice Stuff/Tools.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Program
+{
+
+    public class Tools
+    {
+
+        public static string StrGetResponse()
+        {
+            string strSubmission = Console.ReadLine();
+
+            while (strSubmission == "")
+            {
+                Console.Write("Please input a valid string: ");
+                strSubmission = Console.ReadLine();
+            }
+
+            return strSubmission;
+        }
+
+        public static int IntGetResponse()
+        {
+            int intSubmission;
+
+            string strSubmission = Console.ReadLine();
+
+            bool success = int.TryParse(strSubmission, out int result);
+
+            while (!success)
+            {
+                Console.Write("Please input a whole number: ");
+                strSubmission = Console.ReadLine();
+                success = int.TryParse(strSubmission, out result);
+                
+            }
+            
+            intSubmission = Convert.ToInt32(strSubmission);
+            return intSubmission;
+        }
+
+    }
+
+}
