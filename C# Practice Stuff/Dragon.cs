546c11917519132de090acc652170c93e8e13796
Ben L <veloswim@gmail.com>
2023-02-07 22:56:18 -0800

    merge conflict test

--- C# Practice Stuff/Dragon.cs
+++ C# Practice Stuff/Dragon.cs
@@ -5,18 +5,25 @@ namespace Program
     public class Dragon
     {
         //Properties
-        public string Name {get; private set;}
-        public string Color {get; protected set;}
-        public int Age {get; set;}
-        public string Size {get; set;}
+        public string Name { get; private set; }
 
-        public Dragon(string name, string color, int age = 0, string size = "Small")
+        public string Color { get; protected set; }
+
+        public int Age { get; set; }
+
+        public string Size { get; set; }
+
+        public Dragon(
+            string name,
+            string color,
+            int age = 0,
+            string size = "Small"
+        )
         {
             Name = name;
             Color = color;
             Age = age;
             Size = size;
-
         }
 
         public string Roar()
@@ -30,12 +37,11 @@ namespace Program
 
             int result;
             Random random = new Random();
-            result = random.Next(1,7);
+            result = random.Next(1, 7);
 
-            Console.WriteLine($"{Name} deals {result} to {target}!");
+            Console.WriteLine($"{Name} deals {Name} to {target}!");
 
             Console.WriteLine("Hey check it out, you merged from main!");
         }
-
     }
 }
