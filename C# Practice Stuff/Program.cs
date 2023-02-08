bfb28ab9934ee9182ef4e45a5850fee1ca6c0064
Stephan DeWitz <mudkipman98@gmail.com>
2023-02-07 22:01:06 -0800

    Created the intro, narrator asks the user to enter their information, then builds an instance of a Dragon object with that info.

--- C# Practice Stuff/Program.cs
+++ C# Practice Stuff/Program.cs
@@ -7,7 +7,48 @@ namespace Program
     {
         public static void Main(string[] args)
         {
-            Console.Write("Hello World!");
+            Console.WriteLine("You are a dragon.  Tell me about yourself.");
+
+            //Prompt the user for the stats that will be used to create their dragon.
+
+            Console.Write("What is your name? ");
+            string name = Tools.StrGetResponse();
+            Console.Write($"Hello, {name}.  What color are your scales? ");
+            string color = Tools.StrGetResponse();
+            Console.WriteLine($"Ah, {color}.  A fine color indeed.");
+            Console.Write("How old are you? ");
+            int age = Tools.IntGetResponse();
+            string size;
+
+            switch (age)
+            {
+                case 0:
+                    Console.WriteLine("Oh my!  Newly hatched, I see.  Be careful, young one.");
+                    size = "Small";
+                    break;
+                case <= 5:
+                    Console.WriteLine("Why, you've barely seen what the world has to offer.  Tread lightly, little one.");
+                    size = "Medium";
+                    break;
+                case > 5:
+                    Console.WriteLine("Thank you.");
+                    size = "Large";
+                    break;
+                default:
+            }
+
+            Console.WriteLine("One moment while I understand you."); //Clunky phrasing, consider revising.
+
+            Dragon user = new Dragon(name, color, age, size);
+
+            //uncomment the below and fill in the argument for quick debugging
+            Console.WriteLine(user.Name);
+            Console.WriteLine(user.Color);
+            Console.WriteLine(user.Age);
+            Console.WriteLine(user.Size);
+
+
+
 
         }
     }
