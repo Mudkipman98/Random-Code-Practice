18ce5e31f1c46c70c2fbfef749885aa4d683ca6f
Stephan DeWitz <mudkipman98@gmail.com>
2023-02-17 19:28:32 -0800

    Finished copying a very basic parser.  Will add more complexity later.

--- C# Practice Stuff/Parser.cs
+++ C# Practice Stuff/Parser.cs
@@ -10,15 +10,51 @@ namespace Program
     class Parser
     {
 
-        public static string InputParser(string[] input)
+        public static void InputParser(List<string> input)
         {
             string verb;
             string noun;
             List<string> commands = new List<string> {"take", "drop"};
+            List<string> objects = new List<string> {"flower"};
 
 
-            string output = "";
-            return output;
+            if (input.Count != 2)
+            {
+                Console.WriteLine("Only 2 word commands allowed!");
+            }
+            else
+            {
+                verb = input[0];
+                noun = input[1];
+                if (!commands.Contains(verb))
+                {
+                    Console.WriteLine(verb + " is not a known verb!");
+                }
+                if (!objects.Contains(noun))
+                {
+                    Console.WriteLine(noun + " is not a known noun!");
+                }
+            }
+        }
+
+        public static string RunCommand(string strInput)
+        {
+            char[] delims = {' ', '.'};
+            List<string> strList;
+            string s = "Okay.";
+            string strLow = strInput.Trim().ToLower();
+
+            if (strLow != "q")
+                if (strLow == "") 
+                {
+                    s = "You must enter a command.";
+                }
+                else
+                {
+                    strList = new List<string>(strLow.Split(delims, StringSplitOptions.RemoveEmptyEntries));
+                    InputParser(strList);
+                }
+                return s;
         }
 
     }
